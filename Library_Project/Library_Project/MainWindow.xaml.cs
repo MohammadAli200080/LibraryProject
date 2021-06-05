c1099cdde7cd2fba586a32f139c0f71237f0dec7
kamyar-2020 <kamyarmorad1381@gmail.com>
2021-06-06 00:38:50 +0430

    Fixed Bugs on MainWindow.xaml

--- Library_Project/Library_Project/MainWindow.xaml.cs
+++ Library_Project/Library_Project/MainWindow.xaml.cs
@@ -12,8 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Library_Project.Resources.Windows;
 
-namespace Library_Project.Resources.Windows
+namespace Library_Project
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
