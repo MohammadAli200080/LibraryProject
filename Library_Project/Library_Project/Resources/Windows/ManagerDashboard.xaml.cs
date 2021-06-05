f5636e5d033c913a865f530e6902501c29d56075
kamyar-2020 <kamyarmorad1381@gmail.com>
2021-06-05 19:04:38 +0430

    Added ManagerDashboard.xaml

---
+++ Library_Project/Library_Project/Resources/Windows/ManagerDashboard.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library_Project.Resources.Windows
+{
+    /// <summary>
+    /// Interaction logic for ManagerDashboard.xaml
+    /// </summary>
+    public partial class ManagerDashboard : Window
+    {
+        public ManagerDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void HomePn_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            HomePan.Visibility = Visibility.Visible;
+            BookPan.Visibility = Visibility.Collapsed;
+        }
+
+        private void BooksPn_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            HomePan.Visibility = Visibility.Collapsed;
+            BookPan.Visibility = Visibility.Visible;
+        }
+    }
+}
