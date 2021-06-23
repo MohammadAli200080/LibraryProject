d25e6022cf6fc5fe24f612aa59bc343f8bf859d4
kamyar-2020 <kamyarmorad1381@gmail.com>
2021-06-23 16:48:31 +0430

    Create ImageControl.cs

---
+++ Library_Project/Library_Project/Resources/Classes/ImageControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Library_Project.Resources.Classes
+{
+    class ImageControl
+    {
+        public static ImageSource ByteToImage(string imageDataStr)
+        {
+            byte[] imageData = Encoding.ASCII.GetBytes(imageDataStr);
+
+            BitmapImage biImg = new BitmapImage();
+            MemoryStream ms = new MemoryStream(imageData);
+            biImg.BeginInit();
+            biImg.StreamSource = ms;
+            biImg.EndInit();
+
+            ImageSource imgSrc = biImg as ImageSource;
+
+            return imgSrc;
+        }
+        public static string ImageToByte(FileStream fs)
+        {
+            byte[] imgBytes = new byte[fs.Length];
+            fs.Read(imgBytes, 0, Convert.ToInt32(fs.Length));
+            string encodeData = Convert.ToBase64String(imgBytes, Base64FormattingOptions.InsertLineBreaks);
+
+            return encodeData;
+        }
+    }
+}
