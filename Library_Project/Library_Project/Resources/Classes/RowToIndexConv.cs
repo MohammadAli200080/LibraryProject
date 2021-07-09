9bbfa200ead5541849fb04cebf314891120418ab
kamyar-2020 <kamyarmorad1381@gmail.com>
2021-07-09 14:50:27 +0430

    Add style to datagrids

--- Library_Project/Library_Project/Resources/Classes/RowToIndexConv.cs
+++ Library_Project/Library_Project/Resources/Classes/RowToIndexConv.cs
@@ -4,21 +4,44 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Markup;
 using System.Windows.Media;
 
 namespace Library_Project.Resources.Classes
 {
-    public class RowToIndexConv
+    public abstract class ConverterMarkupExtension<T> : MarkupExtension, IValueConverter where T : class, new()
+    {
+        private static T _converter = null;
+
+        public ConverterMarkupExtension()
+        {
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _converter ?? (_converter = new T());
+        }
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+    }
+    [ValueConversion(typeof(int), typeof(DataGridRow))]
+    public class RowToIndexConv : ConverterMarkupExtension<RowToIndexConv>
     {
         #region IValueConverter Members
 
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            DataGridRow row = value as DataGridRow;
-            return row.GetIndex() + 1;
+            if (value != null && value is DataGridRow)
+            {
+                DataGridRow row = value as DataGridRow;
+                return row.GetIndex() + 1;
+            }
+            return 0;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
